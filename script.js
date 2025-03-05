3ce65feee1b2e569f74f9c43e59bc4a76c83905f
hanyariz <hanyarizvi16@gmail.com>
2025-03-05 13:54:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// Accordion Functionality for Extracurriculars
+let accordions = document.querySelectorAll(".accordion");
+
+accordions.forEach(button => {
+    button.addEventListener("click", function() {
+        let panel = this.nextElementSibling;
+        panel.style.display = (panel.style.display === "block") ? "none" : "block";
+    });
+});
